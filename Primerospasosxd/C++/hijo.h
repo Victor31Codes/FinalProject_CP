0e2f774183b267a5debe21a0fa52bcc8be2a5e27
Victicor_Torres <128977844+Victor31Codes@users.noreply.github.com>
2023-11-24 10:22:32 -0500

    Add files via upload

---
+++ Primerospasosxd/C++/hijo.h
@@ -0,0 +1,14 @@
+#ifndef HIJO_H
+#define HIJO_H
+using namespace std;
+
+class Hijo {
+	public:
+    int id_empleado;
+    int id_hijo;
+    string nombre_hijo;
+	string nac_hijo;
+    
+};
+
+#endif
